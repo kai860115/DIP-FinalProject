0fcb22ae97464df38c50c118c5888576585c2cd1
kai <kai860115@gmail.com>
2022-01-11 23:25:41 +0800

    finsh mask generator

---
+++ gen_mask.py
@@ -0,0 +1,32 @@
+import os
+import argparse
+import cv2
+import numpy as np
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='depthInpainting')
+
+    parser.add_argument('--input', default='', type=str)
+    parser.add_argument('--output_mask', default='./mask.png', type=str)
+    parser.add_argument('--output_missing', default='./missing.png', type=str)
+    parser.add_argument('--missing_rate', default=0.5, type=int)
+
+    args = parser.parse_args()
+    return args
+
+def main(args):
+    img = cv2.imread(args.input, flags=cv2.IMREAD_GRAYSCALE)
+    mask = (np.random.uniform(0, 1, size=img.shape) > args.missing_rate).astype(np.uint8)
+    missing = img * mask
+
+    os.makedirs(os.path.dirname(args.output_mask), exist_ok=True)
+    os.makedirs(os.path.dirname(args.output_missing), exist_ok=True)
+
+    cv2.imwrite(args.output_mask, mask * 255)
+    cv2.imwrite(args.output_missing, missing)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
