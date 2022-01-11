1bdcd415bef030bc5c90411ce383021502db1507
Jacky Tseng <csps50404@gmail.com>
2022-01-11 22:44:29 +0800

    update the get_holes fundtion in HF

--- hole_filling.py
+++ hole_filling.py
@@ -12,8 +12,8 @@ class HF:
     def get_original(self):
       return self.original
 
-    def get_holes(self,img, threshold):
-      return cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY_INV)[1]
+    def get_holes(self):
+      return cv2.threshold(self.original, self.threshold, 255, cv2.THRESH_BINARY_INV)[1]
 
     def dilate(self, src, itr):
       kernel = np.ones(self.dilate_kernel_size, np.uint8)
@@ -21,7 +21,6 @@ class HF:
 
     def get_cluster(self, img):
       label = cv2.connectedComponents(img)[1]
-
       return label
 
     def calc_filling_color(self, img, filter):
@@ -33,7 +32,7 @@ class HF:
     def repair(self):
       repaired = np.copy(self.original)
 
-      holes = self.get_holes(self.original, self.threshold)
+      holes = self.get_holes()
       holes = self.dilate(holes, self.hole_dilate_iter)
       num_label, holes_label = cv2.connectedComponents(holes)
 
